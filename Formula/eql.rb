a3ebc8c31acea16d36bdce3ea765a1b4f686d239
Will Acton <will@willacton.com>
2022-08-08 14:21:06 -0700

    no sha

--- Formula/eql.rb
+++ Formula/eql.rb
@@ -3,7 +3,6 @@ class Eql < Formula
   homepage ""
   license ""
   url "https://github.com/lilactown/eql-cli/archive/v0.0.1.zip"
-  sha256 "3599d11c88a44e8fb41e8da61db53f62d4f5290f"
 
   version "0.0.1"
 
