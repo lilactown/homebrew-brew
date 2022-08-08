314c117a57ef087437e06fc26e313c4a28574f7e
Will Acton <will@willacton.com>
2022-08-08 14:44:51 -0700

    bump to 0.0.2

--- Formula/eql.rb
+++ Formula/eql.rb
@@ -2,9 +2,9 @@ class Eql < Formula
   desc ""
   homepage ""
   license ""
-  url "https://github.com/lilactown/eql-cli/archive/v0.0.1.zip"
+  url "https://github.com/lilactown/eql-cli/archive/v0.0.2.zip"
 
-  version "0.0.1"
+  version "0.0.2"
 
   depends_on "borkdude/brew/babashka"
 
