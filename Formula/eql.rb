5e21f5c72493abb7e87144cdb45b6417b81a7301
Will Acton <will@willacton.com>
2022-08-08 14:19:01 -0700

    correct eql-cli URL

--- Formula/eql.rb
+++ Formula/eql.rb
@@ -2,7 +2,7 @@ class Eql < Formula
   desc ""
   homepage ""
   license ""
-  url "https://github.com/lilactown/eql/archive/v0.0.1.zip"
+  url "https://github.com/lilactown/eql-cli/archive/v0.0.1.zip"
   sha256 "1c3d76acea2b871e329f076f100ab378e80f8430"
 
   version "0.0.1"
